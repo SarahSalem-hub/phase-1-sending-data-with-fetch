3861cd4c409e6238e7a7662614b134961dec2a65
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-09 20:06:26 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+function submitData(userName, userEmail){
+    const UserInfo = {
+        name : userName,
+        email : userEmail
+    }
+   const request = {
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(UserInfo)
+   }
+   return fetch("http://localhost:3000/users", request)
+         .then(res => res.json())
+         .then(function(object){
+
+            let h3 = document.createElement("h3")
+            h3.innerHTML= `${object.name}  ${object.id}` 
+            document.querySelector("body").appendChild(h3)
+         })
+         .catch(function(error){
+            document.body.innerHTML = error
+         })
+}
+submitData("sarah","sarah@mail.com")
